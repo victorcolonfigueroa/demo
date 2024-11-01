101afb14029f23c9895c26b505a886305fe8c4d2
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-10-31 22:01:23 -0400

    worked on readme

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,9 +35,11 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Chef Book</Link>
+                    <Link href="/">
+                      <div className="text-6xl text-yellow-600">Chef Book</div>
+                    </Link>
                     <Button asChild>
-                      <Link href={"/sign-in"}>Sign-in</Link>
+                      {/* <Link href={"/sign-in"}>Sign-in</Link> */}
                     </Button>
                   </div>
                 </div>
